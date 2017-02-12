2d330e49d7d97dedf9860112428d3a45d8e0ad13
seladore <mbdesai@umich.edu>
2017-02-12 09:31:55 -0500

    added author and testing for part 3
    to allow for mutiple versions of the same file

--- social_network_recommender.py
+++
@@ -1,7 +0,0 @@
-import csv
-import networkx as nx
-# Networkx is a package on github which allows for social graph analysis
-# HELLO GROUP 11
-# Guide of social netowrks and recommendations systems
-# http://courses.cs.washington.edu/courses/cse140/14wi/homework/hw4/homework4.html
-social = csv.reader()
