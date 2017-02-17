c85e020e8bfa10ea5de69fd1f9fb6b2816356e48
seladore <mbdesai@umich.edu>
2017-02-17 12:39:40 -0500

    working

---
+++ part_3/adjust_output.py
@@ -0,0 +1,40 @@
+import csv
+
+with open("part_3_testing_data.txt", "rb") as connect:
+    # connections = csv.reader(connect, delimiter="\n")
+    original = connect.readlines()
+    nodes = []
+    for line in original:
+    	node = line.split("\t")[0].replace("\r","").replace("\n","")
+    	nodes.append(int(node))
+        # nodes = [line[0] for line.split("\t") in original]
+    nodes = sorted(nodes)
+    # print nodes
+# print nodes[:20]
+
+with open("counts.txt", "rb") as results:
+	lines = results.readlines()
+	output = {}
+	for result in lines:
+		result = result.replace("\r","").replace("\n","")
+		node, recommended = result.split("\t")
+		# print node, "\t", recommended.replace("[","").replace("]","")
+		output[int(node)] = recommended.replace("[","").replace("]","")
+	# print output
+		# output[]
+	# for line in lines:
+	# 	print line.split("\t")[0], line.split("\t")[1]
+# for node in nodes:
+# 	if node in output:
+# 		print node, '\t', output[node]
+# 	else:
+# 		print node
+
+with open("output_testing_data.txt", "w") as write_output:
+	for node in nodes:
+		if node in output:
+			# print node, output[node]
+			write_output.write("{}\t{}\n".format(node, output[node]))
+		else:
+			# print node
+			write_output.write("{}\n".format(node))
